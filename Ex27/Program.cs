d9e0252c3716b5f128c5058dc2f66526a42c9b92
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-05-01 20:39:13 +0300

    27

---
+++ Ex27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int Result = 1;
+for(int i = 1; i <= a; i++)
+{ 
+    Result = Result * i;
+}
+Console.WriteLine(Result);
