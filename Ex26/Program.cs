d8861847323f280e155d9e02ff306afe4144f3c4
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-05-01 20:38:55 +0300

    26

---
+++ Ex26/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход число и выдаёт количество цифр в числе
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+while(a > 10)
+{
+    a = a / 10;
+    b++;
+}
+Console.Write(b);
