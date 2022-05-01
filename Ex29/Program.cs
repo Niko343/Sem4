52ea6b8ea6779ca323d3fb91e88c5f83c84b4820
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-05-01 22:07:19 +0300

    27dz

---
+++ Ex29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+//из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2,
+void FillArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+void PrintArray(int[]array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+int[]mass = new int[8];
+ FillArray(mass);
+ PrintArray(mass);
