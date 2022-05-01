4dbf3a4b350972e73de6ce77444a8fb0c46b80fa
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-05-01 21:57:10 +0300

    27dz

---
+++ ex27DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+//вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+void PrintSum(int a)
+{
+    int sum = 0;
+    while(a > 0)
+    {
+        int b = a % 10;
+        sum = sum + b;
+        a = (a - b) / 10;
+    }
+    Console.Write(sum);
+}
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+PrintSum(a);
